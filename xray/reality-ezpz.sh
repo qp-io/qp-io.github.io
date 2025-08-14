a4f144f1017c33a39fa4ac32dc65007d6454d7be
qp-io <qphelper@yahoo.com>
2025-08-14 15:59:33 +0300

    Add files via upload

--- xray/reality-ezpz.sh
+++ xray/reality-ezpz.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-# 
-#   http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
 set -e
 declare -A defaults
 declare -A config_file
@@ -978,7 +961,7 @@ EOF
 }
 
 function download_tgbot_script {
-  curl -fsSL -m 3 https://raw.githubusercontent.com/aleskxyz/reality-ezpz/master/tgbot.py -o "${path[tgbot_script]}"
+  curl -fsSL -m 3 https://raw.githubusercontent.com/qp-io/qp-io.github.io/refs/heads/main/xray/tgbot.py -o "${path[tgbot_script]}"
 }
 
 function generate_selfsigned_certificate {
