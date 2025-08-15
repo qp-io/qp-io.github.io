0a7030a47acc70d633b829c0556870d341eb0a83
qp-io <qphelper@yahoo.com>
2025-08-15 14:29:30 +0300

    Add files via upload

--- xray/reality-ezpz.sh
+++ xray/reality-ezpz.sh
@@ -706,7 +706,7 @@ function install_docker {
     docker_cmd="docker-compose"
     return 0
   fi
-  curl -fsSL -m 30 https://github.com/docker/compose/releases/download/v2.28.0/docker-compose-linux-$(uname -m) -o /usr/local/bin/docker-compose
+  curl -fsSL -m 30 https://github.com/docker/compose/releases/download/v2.39.1/docker-compose-linux-$(uname -m) -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
   docker_cmd="docker-compose"
   return 0
